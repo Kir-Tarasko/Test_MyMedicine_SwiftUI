e9cc08352b01dcb2ab8b763eb3a776ef94c68495
KirTarasko <kir.tarasko@gmail.com>
2022-08-21 17:43:39 +0200

    Initial Commit

---
+++ MyMedicine_SwiftUI/MyMedicine_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMedicine_SwiftUIApp.swift
+//  MyMedicine_SwiftUI
+//
+//  Created by Kirill Tarasko on 21.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyMedicine_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
